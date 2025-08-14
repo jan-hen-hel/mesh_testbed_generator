e25f719b76ca410f80dedc6a06c5e36ff9ee1346
yanosz <freifunk@yanosz.net>
2025-08-14 16:05:59 +0200

    Refactor network config

--- utils/generate_wg_conf.rb
+++ utils/generate_wg_conf.rb
@@ -9,7 +9,23 @@ node_data = {}
 
 node_files.each do |nf|
   data = YAML.load_file(nf)
-  node_data.merge! data.nodes
+  data.delete 'build'
+  node_data.merge! data
 end
 
-key_data = YAML.load_file(secrets.yml)
+key_data = YAML.load_file('secrets.yml')['wireguard_keys']
+
+@nodes = []
+node_data.each_pair do |k,v|
+  name = v['hostname']
+  #puts v
+  ip = "198.19.250.2#{v['apu_id']}"
+  wg_key = key_data[name]
+  #puts "echo #{wg_key} | wg pubkey"
+  pubkey = `echo #{wg_key} | wg pubkey`.chomp
+  @nodes << {name: name, ipv4: ip, pubkey: pubkey }
+end
+#puts key_data
+
+template = ERB.new File.new('utils/wireguard.conf.erb').read
+puts template.result
