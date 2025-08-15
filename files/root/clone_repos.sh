1debbeb77f5c8bd328b58b43b1c0d82a8182b6c2
yanosz <freifunk@yanosz.net>
2025-08-15 22:16:11 +0200

    Fixed clone script

--- files/root/clone_repos.sh
+++ files/root/clone_repos.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-git clone -q https://github.com/jan-hen-hel/gnss-lab-dev /data/gnss-lab-dev
-git clone -q https://github.com/jan-hen-hel/gnss-lab /data/gnss-lab
+git clone -q git@github.com:jan-hen-hel/gnss-lab-dev.git /data/gnss-lab-dev
+git clone -q git@github.com:jan-hen-hel/gnss-lab-dev.git /data/gnss-lab
