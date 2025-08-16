c4379fae184e92c7b0543d7eb7791df8bf82bce9
yanosz <freifunk@yanosz.net>
2025-08-16 17:56:15 +0200

    Fix clone problemes

--- files/root/clone_repos.sh
+++ files/root/clone_repos.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 
-git clone -q git@github.com:jan-hen-hel/gnss-lab-dev.git /data/gnss-lab-dev
-git clone -q git@github.com:jan-hen-hel/gnss-lab-dev.git /data/gnss-lab
+git config --global user.name "<%= @node['hostname']%>"
+git config --global user.email "<%= @node['hostname']%>@gnss.dev"
+
+git clone -q git@github.com:jan-hen-hel/gnss-lab-dev.git /ssd/gnss-lab-dev
+git clone -q git@github.com:jan-hen-hel/gnss-lab.git /ssd/gnss-lab
